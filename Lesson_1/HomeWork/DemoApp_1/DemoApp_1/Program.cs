3f696ed843698cd830687e3ed7aba58bd919b69a
ssharshatov <ssharshatov@yandex.ru>
2019-02-20 12:25:35 -0800

    Done home work

---
+++ Lesson_1/HomeWork/DemoApp_1/DemoApp_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApp_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите имя");
+			String name = Console.ReadLine();
+			Thread.Sleep(5000);
+			Console.WriteLine($"Здравствуйте, {name}!");
+			Thread.Sleep(2000);
+			Console.WriteLine($"Спасибо за внимание. Ждем новых встреч, {name}!");
+			Console.ReadKey();
+		}
+	}
+}
