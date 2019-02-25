71d5bced75f88ba887fbe7e8ec63cbd41ceb7271
ssharshatov <ssharshatov@yandex.ru>
2019-02-25 18:45:43 +0300

    Test

---
+++ Lesson_1/ClassWork2/DemoApp_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace DemoApp_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите имя");
+			String name = Console.ReadLine();
+			Thread.Sleep(5000);
+			Console.WriteLine($"Здравствуйте, {name}!");
+			Console.ReadKey();
+		}
+	}
+}
