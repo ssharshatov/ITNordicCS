2978dfe09b4cbcd5a00e9f12f2a0511fbd2b5c0d
ssharshatov <ssharshatov@yandex.ru>
2019-02-26 23:55:17 +0300

    Commit Lesson home work v2

--- Lesson_2/HomeWork/DemoApp/DemoApp/Program.cs
+++ Lesson_2/HomeWork/DemoApp/DemoApp/Program.cs
@@ -9,29 +9,41 @@ namespace DemoApp
 			Console.WriteLine("Добро пожаловать в приложение 'Калькулятор'");
 
 			char[] typesList = new char[3] { 'd', 'i', 'f' };
-			char typeFirstNumber='t';
+			char typeFirstNumber;
 
 			Console.WriteLine("Введите тип данных для первого числа (d-double;i-integer;f-float):");
 
-			while (char.TryParse(Console.ReadLine(), out typeFirstNumber)) {
+			while (!char.TryParse(Console.ReadLine(), out typeFirstNumber))
+			{
 				Console.WriteLine("Введите тип данных для первого числа (d-double;i-integer;f-float):");
-
-				while (!Array.Exists(typesList, element => element == typeFirstNumber))
+				do
 				{
-					Console.WriteLine("Вы ввели не корректный тип данных: ");
+					Console.WriteLine("Вы ввели не корректный тип данных, повторите попытку: ");
 					break;
-				}
+				} while (!Array.Exists(typesList, element => element == typeFirstNumber));
 			}
 
-			if (typeFirstNumber == 'd') {
+			if (typeFirstNumber == 'd')
+			{
 				Console.WriteLine("Введите первое число:");
-				double firstNumber;
+				var firstNumber = 34.34;
 				while (!double.TryParse(Console.ReadLine(), out firstNumber))
-					Console.WriteLine("Введит тип данных Double, повторите ввод");
+					Console.WriteLine("Введите тип данных Double, повторите ввод");
 			}
-				else if (typeFirstNumber == 'i')
-			{ }
-
+			else if (typeFirstNumber == 'i')
+			{
+				Console.WriteLine("Введите первое число:");
+				int firstNumber = 34;
+				while (!int.TryParse(Console.ReadLine(), out firstNumber))
+					Console.WriteLine("Введите тип данных Integer, повторите ввод");
+			}
+			else if (typeFirstNumber == 'f')
+			{
+				Console.WriteLine("Введите первое число:");
+				float firstNumber = 34.34f;
+				while (!float.TryParse(Console.ReadLine(), out firstNumber))
+					Console.WriteLine("Введите тип данных Float, повторите ввод");
+			}
 		}
 	}
 }
